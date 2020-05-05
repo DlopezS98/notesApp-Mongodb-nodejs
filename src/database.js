e0f4841fc1ffd40ac7182eb74f38a0c87ba80eef
Danny Lopez <01dlopezS98@gmail.com>
2020-05-04 23:19:03 -0600

    creando la conexion a la base de datos

---
+++ src/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env; //Trayendo la cadena de conexion desde nuestras variables de entorno
+const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`;
+
+//Creando la conexion a mongodb
+mongoose.connect(MONGODB_URI,{
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}).then(db => console.log('Database is connected')).catch(err => console.log(err));
