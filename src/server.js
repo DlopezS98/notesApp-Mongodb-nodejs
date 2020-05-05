c56a8164202e806a3376c097f625fdb742ad9428
Danny Lopez <01dlopezS98@gmail.com>
2020-05-05 09:29:07 -0600

    configurando el motor de plantillas handlebars en el servidor

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 //Initialization
@@ -7,6 +8,15 @@ const app = express();
 //Settings
 app.set('port', process.env.PORT || 4000); //obteniendo un puerto para el servidor
 app.set('views', path.join(__dirname, 'views')); //Obteniendo la ruta de la carpeta "views"
+//creando la configuracion para el motor de plantillas handlebars
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+}));
+app.set('view engine', '.hbs');
+
 
 //Middlewares
 app.use(express.urlencoded({extended : false})); //Permite que cada vez que lleguen datos atravez de cualquier tipo de metodo o peticion, convertir esos datos en un objeto json
@@ -15,8 +25,8 @@ app.use(express.urlencoded({extended : false})); //Permite que cada vez que lleg
 
 //Routes
 app.get('/', (req, res) => {
-    res.send('hello world');
-})
+    res.render('index');
+});
 
 //Statics file
 /* Diciendo al servidor donde se localiza la carpeta "public" */
